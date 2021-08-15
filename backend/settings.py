e1876b585964c6bec3203fcc4314848f688eee6f
Astig Mandalian <astigmandalian@Astigs-MacBook-Air.local>
2021-08-15 12:21:00 +0100

    Added personal comments to point out the databases

--- backend/settings.py
+++ backend/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3', # these are our databases that we use for the project
+        'NAME': BASE_DIR / 'db.sqlite3',           
     }
 }
 
